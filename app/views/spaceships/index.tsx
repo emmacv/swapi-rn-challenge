388165a33b85c4618b215ec8a2f86af0ddb18cfd
Emmanuel Canto Vazquez <emikogears134@gmail.com>
2024-07-25 14:52:33 -0600

    feat: Add navigation.

---
+++ app/views/spaceships/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SpaceshipView = () => {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  )
+}
+
+export default SpaceshipView
